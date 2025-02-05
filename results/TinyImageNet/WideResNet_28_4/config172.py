4124f5fd76558496ab45c25f18cd3d09ae13c8c6
Georg Siedel <georgsiedel@gmx.de>
2025-02-05 14:16:50 +0000

    tensor stylized dataset

--- results/TinyImageNet/WideResNet_28_4/config172.py
+++ results/TinyImageNet/WideResNet_28_4/config172.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 train_corruptions = np.array([
-#{'noise_type': 'standard', 'epsilon': 0.0, 'sphere': False, 'distribution': 'beta2-5'},
-{'noise_type': 'uniform-linf', 'epsilon': 0.2, 'sphere': False, 'distribution': 'uniform'},
-{'noise_type': 'uniform-l0.5', 'epsilon': 12000000.0, 'sphere': False, 'distribution': 'uniform'},
-{'noise_type': 'uniform-l1', 'epsilon': 1500.0, 'sphere': False, 'distribution': 'uniform'},
-{'noise_type': 'uniform-l2', 'epsilon': 20.0, 'sphere': False, 'distribution': 'uniform'},
-{'noise_type': 'uniform-l0-impulse', 'epsilon': 0.4, 'sphere': False, 'distribution': 'uniform'},
+{'noise_type': 'standard', 'epsilon': 0.0, 'sphere': False, 'distribution': 'beta2-5'},
+#{'noise_type': 'uniform-linf', 'epsilon': 0.1, 'sphere': False, 'distribution': 'uniform'},
+#{'noise_type': 'uniform-l0.5', 'epsilon': 400000.0, 'sphere': False, 'distribution': 'uniform'},
+#{'noise_type': 'uniform-l1', 'epsilon': 200.0, 'sphere': False, 'distribution': 'uniform'},
+#{'noise_type': 'uniform-l2', 'epsilon': 5.0, 'sphere': False, 'distribution': 'uniform'},
+#{'noise_type': 'uniform-l0-impulse', 'epsilon': 0.2, 'sphere': False, 'distribution': 'uniform'},
 ])
 noise_sparsity = 1.0
 noise_patch_scale = {'lower': 0.2, 'upper': 0.7}
@@ -22,20 +22,20 @@ validontest = True
 validonc = True
 validonadv = False
 lrschedule = 'CosineAnnealingWarmRestarts'
-learningrate = 0.0001
-epochs = 20
+learningrate = 0.1
+epochs = 300
 lrparams = {'T_0': 20, 'T_mult': 2}
 warmupepochs = 0
 earlystop = False
 earlystopPatience = 15
-optimizer = 'AdamW'
-optimizerparams = {'weight_decay': 0.05}
+optimizer = 'SGD'
+optimizerparams = {'momentum': 0.9, 'weight_decay': 1e-4, 'nesterov': True}
 number_workers = 2
-modeltype = 'ViT_B_16'
-modelparams = {'weights': 'IMAGENET1K_V1'}
-resize = True
+modeltype = 'WideResNet_28_4'
+modelparams = {'dropout_rate': 0.2, 'activation_function': 'silu'}
+resize = False
 aug_strat_check = True
-train_aug_strat_orig = 'TrivialAugmentWide' #TrivialAugmentWide, RandAugment, AutoAugment, AugMix
+train_aug_strat_orig = 'StyleTransfer100' #TrivialAugmentWide, RandAugment, AutoAugment, AugMix
 train_aug_strat_gen = 'TrivialAugmentWide' #TrivialAugmentWide, RandAugment, AutoAugment, AugMix
 loss = 'CrossEntropyLoss'
 lossparams = {'label_smoothing': 0.1}
@@ -46,8 +46,8 @@ robust_lossparams = {'num_splits': 3, 'alpha': 12} #jsd if 3 splits, KL divergen
 mixup = {'alpha': 0.2, 'p': 0.0} #default alpha 0.2 #If both mixup and cutmix are >0, mixup or cutmix are selected by 0.5 chance
 cutmix = {'alpha': 1.0, 'p': 0.0} # default alpha 1.0 #If both mixup and cutmix are >0, mixup or cutmix are selected by 0.5 chance
 manifold = {'apply': False, 'noise_factor': 3}
-RandomEraseProbability = 0.3
-swa = {'apply': True, 'start_factor': 0.7, 'lr_factor': 0.2}
+RandomEraseProbability = 0.0
+swa = {'apply': True, 'start_factor': 0.9, 'lr_factor': 0.2}
 
 #define train and test corruptions:
 #define noise type (first column): 'gaussian', 'uniform-l0-impulse', 'uniform-l0-salt-pepper', 'uniform-linf'. also: all positive numbers p>0 for uniform Lp possible: 'uniform-l1', 'uniform-l2', ...
