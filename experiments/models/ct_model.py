8b97b37689b73094365ecc97f5f32c32ff55a195
RojanRegmi <rznrgmi@gmail.com>
2025-02-20 22:49:32 +0100

    adjusting style feats passing through blocks

--- experiments/models/ct_model.py
+++ experiments/models/ct_model.py
@@ -92,9 +92,13 @@ class CtModel(nn.Module):
             f"[TEST] Style feat type: {type(style_feats)} and out type: {type(out)} and shape of style feats {style_feats.shape}, shape of content {out.shape}"
         )
         out = self.blocks[0](out)
+        if style_feats:
+            style_feats = self.blocks[0](style_feats)
         prob = torch.rand(1).item()
         for i, ResidualBlock in enumerate(self.blocks[1:]):
             out = ResidualBlock(out)
+            if style_feats:
+                style_feats = ResidualBlock(style_feats)
             if k == (i + 1):  # Do manifold mixup if k is greater 0
                 out, targets = mixup_process(
                     out,
@@ -120,8 +124,8 @@ class CtModel(nn.Module):
 
             if self.internal_adain_prob is not None:
                 if prob < self.internal_adain_prob and i == 0:
-                    style_feats = self.blocks[0](style_feats)
-                    style_feats = self.blocks[1](style_feats)
+                    # style_feats = self.blocks[0](style_feats)
+                    # style_feats = self.blocks[1](style_feats)
                     print(
                         f"[TEST] Style feat type: {type(style_feats)} and shape of style feats {style_feats.shape}, shape of content {out.shape}"
                     )
