9ed238f57f7eb0ec3664d212b5a702069cb75a8e
Georgsiedel <georgsiedel@gmx.de>
2025-07-14 18:47:23 +0200

    runs

--- run_exp_1.py
+++ run_exp_1.py
@@ -6,7 +6,7 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 if __name__ == '__main__':
     import importlib
 
-    for experiment in [530,534]:
+    for experiment in [530]:
 
         configname = (f'experiments.configs.config{experiment}')
         config = importlib.import_module(configname)
